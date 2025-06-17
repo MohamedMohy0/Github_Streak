91c3a918aeca4130175d845a24736e00fe0d67d2
Mohamed Mohy <105385056+MohamedMohy0@users.noreply.github.com>
2025-06-17 13:46:27 +0300

    Update Github_Streak.py

--- Github_Streak.py
+++ Github_Streak.py
@@ -27,9 +27,10 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.maximize_window()
 driver.get("https://github.com/")
+time.sleep(2)
 signin=driver.find_element("xpath", '//*[@class="HeaderMenu-link HeaderMenu-link--sign-in HeaderMenu-button flex-shrink-0 no-underline d-none d-lg-inline-flex border border-lg-0 rounded rounded-lg-0 px-2 py-1"]') 
 signin.click()
-
+time.sleep(2)
 user=driver.find_element("xpath", '//*[@id="login_field"]')
 user.send_keys(Email)
 password=driver.find_element("xpath", '//*[@id="password"]')
@@ -37,12 +38,9 @@ password.send_keys(Pass)
 time.sleep(1)
 signin1=driver.find_element("xpath", '//*[@class="btn btn-primary btn-block js-sign-in-button"]') 
 signin1.click()
-
-
-
+time.sleep(1)
 driver.get("https://camo.githubusercontent.com/f709f1852ab978e56d4c6fa3a598216d3cb0b3923519ac0e8762712ff67335f1/68747470733a2f2f6769746875622d726561646d652d73747265616b2d73746174732e6865726f6b756170702e636f6d2f3f757365723d4d6f68616d65644d6f687930267468656d653d746f6b796f6e69676874")
 time.sleep(2)
-
 texts = driver.find_elements(By.TAG_NAME, 'text')
 
 for i,t in enumerate(texts):
